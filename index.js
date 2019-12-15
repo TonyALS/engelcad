b5675203872aaa77a723235565ae9e8ee3830359
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2019-12-15 11:03:53 -0300

    Configure view-engine, body-parser and express

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const connection = require('./database/database');
+const bodyParser = require('body-parser');
+
+//Carrega view engine:
+app.set('view engine', 'ejs');
+
+//Configurar o express para trabalhar com arquivos estáticos:
+app.use(express.static('public'));
+
+//Configure body-parser:
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //Conecta ao banco:
 connection.authenticate().then(() => {
@@ -8,5 +19,15 @@ connection.authenticate().then(() => {
 }).catch((err) => {
     console.log('Erro ao conectar ao banco: ' + err)
 });
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, () => console.log('Servidor em execução.'))
