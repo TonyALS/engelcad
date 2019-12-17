959c2d2918dba7bd9e9d88127a0ec0a4d083180e
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2019-12-16 22:06:05 -0300

    Create router for new page articles visualization

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -4,11 +4,13 @@ const router = express.Router();
 //Carrega o model de Artigos para executar o comando de save:
 const Article = require('./Articles');
 
+//Rota da página de criação de novo artigo:
 router.get('/admin/article/new', (req, res) => {
     res.render('admin/articles/new');
 });
 
-router.post('/articles/save', (req, res) => {
+//Rota post para salvar um novo artigo:
+router.post('/admin/article/save', (req, res) => {
     let title = req.body.title;
     let body = req.body.body;
 
@@ -18,6 +20,17 @@ router.post('/articles/save', (req, res) => {
         body: body
     }).then(() => {
         res.send('Artigo salvo')
+    });
+});
+
+//Rota para carregar os artigos cadastrados:
+router.get('/articles', (req, res) => {
+    Article.findAll({
+        order: [
+            ['createdAt', 'DESC']
+        ]
+    }).then((articles) => {
+        res.render('article', {articles: articles});
     });
 });
 
