c3dfaa1a2ef88838ff9dd5edc038273d0c42f34e
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2019-12-16 21:30:14 -0300

    Implements router from save new articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+//Carrega o model de Artigos para executar o comando de save:
+const Article = require('./Articles');
+
 router.get('/admin/article/new', (req, res) => {
     res.render('admin/articles/new');
+});
+
+router.post('/articles/save', (req, res) => {
+    let title = req.body.title;
+    let body = req.body.body;
+
+    //Salvando artigo:
+    Article.create({
+        title: title,
+        body: body
+    }).then(() => {
+        res.send('Artigo salvo')
+    });
 });
 
 module.exports = router;
